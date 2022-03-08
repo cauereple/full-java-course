44ebeac2e8e0335b68ff85b097abd60bf539256d
Cauê Reple <caue-reple@hotmail.com>
2022-03-08 22:21:52 +0100

    multi catch, stackTrace and throws

--- src/br/com/xti/erros/DividePorZero.java
+++ src/br/com/xti/erros/DividePorZero.java
@@ -4,6 +4,15 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DividePorZero {
+	
+	public static void dividir(Scanner s) {
+		System.out.print("Numero: ");
+		int a = s.nextInt();
+		System.out.print("Divisor: ");
+		int b = s.nextInt();
+	
+		System.out.println(a / b);
+	}
 
 	public static void main(String[] args) {
 		
@@ -14,18 +23,14 @@ public class DividePorZero {
 		do {
 			try {
 				
-				System.out.print("Numero: ");
-				int a = s.nextInt();
-				System.out.print("Divisor: ");
-				int b = s.nextInt();
-			
-				System.out.println(a / b);
+				dividir(s);
 				// se chegar até essa linha de código, mudamos a nossa variável para false
 				// para que possa sair do ciclo
 				continua = false;
 				
 			}
 			catch(InputMismatchException | ArithmeticException e1) {
+				e1.printStackTrace();
 				System.err.println("Número Inválido");
 				s.nextLine(); //descarta a entrada errada e libera novamente para o usuário
 			}
