0009790be48f928848db6cb905530ece88a77941
Cauê Reple <caue-reple@hotmail.com>
2022-03-09 09:10:08 +0100

    creating its own exception

---
+++ src/br/com/xti/erros/SenhaTest.java
@@ -0,0 +1,25 @@
+package br.com.xti.erros;
+
+public class SenhaTest {
+	
+	static void autenticar(String senha) throws SenhaInvalidaException {
+		if("123".equals(senha)) {
+			//autenticado
+			System.out.println("Atenticado");
+		} else {
+			//senha incorreta
+			throw new SenhaInvalidaException("Senha incorreta");
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		try {
+			autenticar("1234");
+		} catch(SenhaInvalidaException e) {
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
