8aa4687e1fe01d40eab33400b51815fcd17704de
Cauê Reple <caue-reple@hotmail.com>
2022-03-11 09:18:18 +0100

    String split

---
+++ src/br/com/xti/java/StringToken.java
@@ -0,0 +1,16 @@
+package br.com.xti.java;
+
+public class StringToken {
+
+	public static void main(String[] args) {
+		
+		String s = "XHTML; CSS; JavaScript; jQuedry; Java";
+		String[] tokens = s.split(";");
+		System.out.println(tokens.length + " tokens");
+		
+		for(String token : tokens) {
+			System.out.println(token);
+		}
+	}
+
+}
