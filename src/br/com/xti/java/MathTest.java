ab09b8814174a7e2aba0e6774e5cb47f1f1fd004
Cauê Reple <caue-reple@hotmail.com>
2022-03-22 19:08:23 +0100

    Class Math

---
+++ src/br/com/xti/java/MathTest.java
@@ -0,0 +1,19 @@
+package br.com.xti.java;
+
+public class MathTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Math.sin(10)); // seno
+		System.out.println(Math.cos(10)); // cosseno
+		System.out.println(Math.tan(45)); // tangente
+		System.out.println(Math.pow(4, 2)); // potência
+		System.out.println(Math.sqrt(16)); // raiz quadrada
+		System.out.println(Math.cbrt(16)); // raiz cúbica
+		System.out.println(Math.floor(11));
+		System.out.println(Math.round(16.5));
+		System.out.println(Math.max(16, 18));
+		System.out.println(Math.min(16, 18));
+	}
+
+}
