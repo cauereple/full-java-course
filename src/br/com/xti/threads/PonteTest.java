16a8138c1d2115f5b1d817c01af197e086ff681a
Cauê Reple <caue-reple@hotmail.com>
2022-03-26 00:07:55 +0100

    Threads wait, notifyAll first step

---
+++ src/br/com/xti/threads/PonteTest.java
@@ -0,0 +1,13 @@
+package br.com.xti.threads;
+
+public class PonteTest {
+
+	public static void main(String[] args) {
+		
+		Ponte ponte = new PonteNaoSincronizada();
+		new Thread(new Produtor(ponte)).start();
+		new Thread(new Consumidor(ponte)).start();
+
+	}
+
+}
