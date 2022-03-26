e1c9d9d579ee824a02695074854ff2b5a92cfed2
Cauê Reple <caue-reple@hotmail.com>
2022-03-26 11:14:11 +0100

    Threads wait, notifyAll final step

--- src/br/com/xti/threads/PonteTest.java
+++ src/br/com/xti/threads/PonteTest.java
@@ -4,7 +4,7 @@ public class PonteTest {
 
 	public static void main(String[] args) {
 		
-		Ponte ponte = new PonteNaoSincronizada();
+		Ponte ponte = new PonteSincronizada();
 		new Thread(new Produtor(ponte)).start();
 		new Thread(new Consumidor(ponte)).start();
 
