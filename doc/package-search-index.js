e4c21b689d4c76501fe95a60cf6919f6e0eaf604
Cauê Reple <caue-reple@hotmail.com>
2022-03-27 14:43:46 +0200

    javadoc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"m":"xti2","l":"br.com.xti.colecao"},{"m":"xti2","l":"br.com.xti.erros"},{"m":"xti2","l":"br.com.xti.heranca"},{"m":"xti2","l":"br.com.xti.java"},{"m":"xti2","l":"br.com.xti.logica"},{"m":"xti2","l":"br.com.xti.poo"},{"m":"xti2","l":"br.com.xti.refinado"},{"m":"xti2","l":"br.com.xti.threads"}]
--- src/br/com/xti/threads/Ponte.java
+++ src/br/com/xti/threads/Ponte.java
@@ -1,8 +1,39 @@
 package br.com.xti.threads;
 
+/**
+ * Uma <code>Ponte</code> representa o elo de ligação
+ * entre objetos Produtores e Consumidores de informações.
+ * Os <i>Produtores</i> utilizam as Pontes para gravar 
+ * informações e compartilhar essas informações com os
+ * Consumidores que lêem esses dados da Ponte para fazer
+ * o processamento. 
+ * 
+ * @author Caue Rizzo Reple
+ * @version 1.3
+ * @since 1.0
+ * 
+ * @see PonteNaoSincronizada
+ * @see PonteSincronizada
+ */
+
 public interface Ponte {
 	
+	/**
+	 * Armazena o valor informado na ponte. Geralmente
+	 * será chamado pelas classes Produtoras de dados.
+	 * 
+	 * @param valor
+	 * @throws InterruptedException
+	 */
 	public void set(int valor) throws InterruptedException;
+	
+	/**
+	 * Recupera a informação armazenada na Ponte.
+	 * Esse método será chamado pelas Consumidoras de dados.
+	 * 
+	 * @return
+	 * @throws InterruptedException
+	 */
 	public int get() throws InterruptedException;
 	
 
