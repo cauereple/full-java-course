1e42493cc192e4ed4004c86ea55528b815db3c1f
Cauê Reple <caue-reple@hotmail.com>
2022-03-28 16:43:59 +0200

    Socket server

--- src/br/com/xti/redes/ConselhosCliente.java
+++ src/br/com/xti/redes/ConselhosCliente.java
@@ -1,15 +1,16 @@
 package br.com.xti.redes;
 
-import java.io.IOException;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class ConselhosCliente {
 
 	public static void main(String[] args) throws Exception {
 		
-		Socket socket = new Socket("127.0.0.1", 5000);
-
+		Socket socket = new Socket("127.0.0.1", 6000);
+		Scanner s = new Scanner(socket.getInputStream());
+		System.out.println("Mensagem: " + s.nextLine());
+		s.close();
 	}
 
 }
